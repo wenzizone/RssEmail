c8170cb62bcce69053892105fcad78b0d1cf666e
Hanzhao Liu <wenzizone@126.com>
2013-04-22 23:11:20 +0800

    初步完成从csv文件发送邮件，不过性能还有待改进，不过可以进入测试阶段了。

--- rssemail.py
+++ rssemail.py
@@ -6,42 +6,61 @@ import sys,os,random
 import smtplib,base64
 
 print len(sys.argv)
-if len(sys.argv) != 6:
-    sys.exit('Usage: rssemail.py emailfile titlefile content1file content2file content3file')
+if len(sys.argv) != 2:
+    sys.exit('Usage: rssemail.py emailfile')
 
 emailfile = sys.argv[1]
-titlefile = sys.argv[2]
-content1file = sys.argv[3]
-content2file = sys.argv[4]
-content3file = sys.argv[5]
 
-def get_random_file_content(file):
-    fh = open(file,'rb')
+
+def get_random_file_content(file,contentposition):
+    fh = open(file, 'rb')
     fn_size = os.stat(file)[6]
     pos = (fh.tell() + random.randint(0,fn_size)) % (fn_size)
+    if contentposition == 't':
+        contentpos = 2
+    elif contentposition == 'ca':
+        contentpos = 3
+    elif contentposition == 'cb':
+        contentpos = 4
+    elif contentposition == 'cc':
+        contentpos =5
+
     try:
         fh.seek(pos)
-        fh.readline()
+        csvfh = csv.reader(fh)
+        csvfh.next()
         while True:
-            line_content = fh.readline()
-            if line_content.split():
+            tmp = csvfh.next()
+            #print len(tmp)
+            if len(tmp) != 8:
+                continue
+            line_content = tmp[contentpos]
+            #print line_content
+            if len(line_content):
                 break
             else:
                 continue
     except EOFError:
-            pass
+        pass
 
-
+    #csvfh.close()
     fh.close()
+    #print line_content
     return line_content
 
-
-mail_title = get_random_file_content(titlefile)
+mail_title = get_random_file_content(emailfile, 't')
+content1 = get_random_file_content(emailfile, 'ca')
+content2 = get_random_file_content(emailfile, 'cb')
+content3 = get_random_file_content(emailfile, 'cc')
 
 # main 
 file = csv.reader(open(emailfile))
 
-for s_email,s_passwd,r_email,r_passwd in file:
+for lines in file:
+    s_email = lines[0]
+    s_passwd = lines[1]
+    r_email = lines[6]
+    r_passwd = lines[7]
     if s_email == '':
         continue
     tmp= s_email.split('@')
@@ -49,18 +68,32 @@ for s_email,s_passwd,r_email,r_passwd in file:
         continue
     domain = tmp[1]
     print s_email,",",domain,",",s_passwd,",",r_email
-    smtpobj = smtplib.SMTP('smtp.%s'%domain)
-    smtpobj.login(s_email,s_passwd)
-    
 
+    message = '''From: %s <%s>
+    To: %s <%s>
+    MIME_version: 1.0
+    Content-type: text/html
+    Subject: %s
 
+    <html>
+    <head>
+    </head>
+    <body>
+    <p>%s</p>
+    <p>%s</p>
+    %s
+    </body>
+    </html>
+    '''%(s_email,s_email,r_email,r_email,mail_title,content1,content2,content3)
+    #print message
+    smtp = smtplib.SMTP()
+    smtp.set_debuglevel(0)
+    smtp.connect('smtp.%s'%(domain))
+    #smtp.helo()
+    smtp.login(s_email,s_passwd)
 
-
-'''while 1:
-    lines = file.readlines(1000)
-    if not lines:
-        break
-    for line in lines:
-        (a,b,c,d,e,f,g,h) = line.split(',')
-        print a
-        '''
+    #smtpobj = smtplib.SMTP('smtp.%s'%(domain), 25)
+    #smtpobj.ehlo()
+    #smtpobj.starttls()
+    #smtpobj.login(s_email,s_passwd)
+    smtp.sendmail(s_email,r_email,message)
